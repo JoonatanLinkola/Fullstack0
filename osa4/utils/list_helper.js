4198b176b582a076d964ed6e80e2f4e237f175e3
Joonatan Linkola <joonatan.linkola@gmail.com>
2022-07-11 21:55:24 +0300

    tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -3,6 +3,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  const likes = blogs.map(each => each.likes)
+
+  return totalLikes.length === 0
+    ? 0
+    : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
