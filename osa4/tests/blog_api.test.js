db00c521ea02cad4842894891c1e0f5dc77943ff
Joonatan Linkola <joonatan.linkola@gmail.com>
2022-07-12 22:39:42 +0300

    tehtävä 4.13

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -85,6 +85,25 @@ test('blog without title or url is not added', async () => {
   expect(blogsAtEnd).toHaveLength(helper.listWithLotsOfBlogs.length)
 })
 
+test('deleting a blog', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.listWithLotsOfBlogs.length - 1
+  )
+
+  const titles = blogsAtEnd.map(n => n.title)
+
+  expect(titles).not.toContain(blogToDelete.content)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
